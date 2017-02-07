ddb778f315f7c967ce43fc821fa06e8ae1032e60
Giuseppe Marzatico <g.marzatico@outlook.com>
2017-02-07 04:46:21 +0100

    Installer creates basic app_local.php file.

--- src/Shell/InstallShell.php
+++ src/Shell/InstallShell.php
@@ -8,19 +8,31 @@ use Cake\Core\Configure;
 use Cake\Core\Configure\Engine\PhpConfig;
 
 class InstallShell extends Shell
-{
+{	
+
     public function main()
     {
-        $this->out('Hello world.');
-		/*
+        $this->out('FortuneCookies installation');
+		$this->hr();
+        $this->out('This procedure will output a file called app_local.php');
+        $this->out('to store Security Salt and Database connection information');
+		
+		// Salt
+		$this->hr();
+		$this->out('Configure Security Salt');
+		$this->out('A Security Salt is a random string used to encrypt sensitive data inside this app.');
+		$salt = $this->in('Type a string:');
+		Configure::write('Security.salt', $salt);
+		// Database\Connection
+		// Admin account
+		// Mail
+		
 		if(!Configure::read('Security.salt'))
 		{
 			$salt = $this->in('Salt:');
-			Configure::write('Security.salt', $salt);
 		}
-		*/
-		Configure::write('debug', true);
-		Configure::write('Security.salt', '1234567890987654321');
+
+		Configure::write('debug', false);
 		Configure::write('Datasources.default.className', 'Cake\Database\Connection');
 		Configure::write('Datasources.default.driver', 'Cake\Database\Driver\Mysql');
 		Configure::write('Datasources.default.persistent', false);
@@ -37,7 +49,9 @@ class InstallShell extends Shell
 		Configure::write('Datasources.default.url', env('DATABASE_URL', null));
 		$file = new File('config/app_local.php', false);
 		if(!$file->exists()) {
-			$this->out(Configure::dump('app_local', 'default'));
+			if($ris = Configure::dump('app_local', 'default')) {
+				$this->out("Wrote config file app_local.php");
+			}
 		}
 		$file->close();
     }
