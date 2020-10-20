ed4dd4cc7114eff7f52e38f3f8dca2108514146b
giumar <info@giumar.net>
2020-10-20 09:58:27 +0200

    Fix: #30 Fatal error: Declaration of App\Controller\DashboardController::initialize()

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -10,7 +10,7 @@ use App\Controller\AppController;
 class DashboardController extends AppController
 {
 
-    public function initialize()
+    public function initialize() : void
     {
         parent::initialize();
 
