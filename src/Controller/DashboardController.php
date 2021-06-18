0ad5516e1ded013ca4ceea634bc43d8da18e71d5
giumar <info@giumar.net>
2021-06-18 06:22:30 +0200

    Riformattazione del codice.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
@@ -7,17 +8,16 @@ use App\Controller\AppController;
  * Dashboard Controller
  *
  */
-class DashboardController extends AppController
-{
+class DashboardController extends AppController {
 
-    public function initialize() : void
-    {
+    public function initialize(): void {
         parent::initialize();
 
         $this->Auth->allow(['index']);
     }
 
-    public function index()
-    {
+    public function index() {
+        
     }
+
 }
